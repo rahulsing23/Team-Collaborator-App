b188da5d34f01a7e3e79bc57c3e178610389f884
rahulsing23 <rahul561singh@gmail.com>
2024-10-06 01:27:37 +0530

    Adding Organization feature

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,10 +1,16 @@
+"use client"
 import React from 'react'
 import Logo from "@/app/_components/Logo"
-import { UserButton } from '@clerk/nextjs'
+import { OrganizationSwitcher, UserButton, useAuth } from '@clerk/nextjs'
 function Header() {
+    const {orgId} = useAuth()
+    console.log(orgId)
   return (
     <div className="flex justify-between items-center p-3 shadow-md">
       <Logo/>
+      <OrganizationSwitcher 
+        afterLeaveOrganizationUrl = {'/dashboard'}
+        afterCreateOrganizationUrl={'/dashboard'} />
       <UserButton/>
     </div>
   )
